581e281e93004f61c7369bd15126992899aac60e
helderscrolls <nortehelder@gmail.com>
2019-08-23 18:20:53 +0200

    added crypto plugin to hash and salt our password, created the setPassword function

--- back/models/User.js
+++ back/models/User.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var uniqueVaildator = require('mongoose-unique-validator');
+var crypto = require('crypto');
 
 var UserSchema = new mongoose.Schema({
   username: { type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true },
@@ -11,5 +12,10 @@ var UserSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 UserSchema.plugin(uniqueVaildator, { message: 'is already taken.' });
+
+UserSchema.methods.setPassword = function(password){
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+}
 
 mongoose.model('User', UserSchema);
