d4cc4cd93a8ca0ff0c6dedb7d8d855ebe7c2b677
helderscrolls <nortehelder@gmail.com>
2019-08-23 18:24:10 +0200

    added method to validate passwords

--- back/models/User.js
+++ back/models/User.js
@@ -13,9 +13,14 @@ var UserSchema = new mongoose.Schema({
 
 UserSchema.plugin(uniqueVaildator, { message: 'is already taken.' });
 
-UserSchema.methods.setPassword = function(password){
+UserSchema.methods.setPassword = function(password) {
   this.salt = crypto.randomBytes(16).toString('hex');
   this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
-}
+};
+
+UserSchema.methods.validPassword = function(password) {
+  var hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512'),toString('hex');
+  return this.hash === hash;
+};
 
 mongoose.model('User', UserSchema);
