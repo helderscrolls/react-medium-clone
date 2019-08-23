7e3ba2500fd169ba0d5268211f7d50044ff66dfe
helderscrolls <nortehelder@gmail.com>
2019-08-23 18:54:26 +0200

    added JWT method  to generate JWT tokens

--- back/models/User.js
+++ back/models/User.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 var uniqueVaildator = require('mongoose-unique-validator');
 var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
+var secret = require('../config').secret;
 
 var UserSchema = new mongoose.Schema({
   username: { type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true },
@@ -13,14 +15,26 @@ var UserSchema = new mongoose.Schema({
 
 UserSchema.plugin(uniqueVaildator, { message: 'is already taken.' });
 
-UserSchema.methods.setPassword = function(password) {
+UserSchema.methods.setPassword = function (password) {
   this.salt = crypto.randomBytes(16).toString('hex');
   this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
 };
 
-UserSchema.methods.validPassword = function(password) {
-  var hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512'),toString('hex');
+UserSchema.methods.validPassword = function (password) {
+  var hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512'), toString('hex');
   return this.hash === hash;
+};
+
+UserSchema.methods.generateJWT = function () {
+  var today = new Date();
+  var exp = new Date(today);
+  exp.setDate(today.getDate() + 60);
+
+  return jwt.sign({
+    id: this._id,
+    username: this.username,
+    exp: parseInt(exp.getTime() / 1000),
+  }, secret);
 };
 
 mongoose.model('User', UserSchema);
