d88820eb97e358a8456ddfa75b569794789139c7
helderscrolls <nortehelder@gmail.com>
2019-08-24 02:35:24 +0200

    added testing json and script for future models/routes and also updated user.js route so it generates the user.bio and the user.image fields on the database

--- back/routes/api/users.js
+++ back/routes/api/users.js
@@ -4,15 +4,38 @@ var passport = require('passport');
 var User = mongoose.model('User');
 var auth = require('../auth');
 
-router.post('/users', function (req, res, next) {
-  var user = new User();
+router.get('/user', auth.required, function (req, res, next) {
+  User.findById(req.payload.id).then(function (user) {
+    if (!user) { return res.sendStatus(401); }
 
-  user.username = req.body.user.username;
-  user.email = req.body.user.email;
-  user.setPassword(req.body.user.password);
-
-  user.save().then(function () {
     return res.json({ user: user.toAuthJSON() });
+  }).catch(next);
+});
+
+router.put('/user', auth.required, function (req, res, next) {
+  User.findById(req.payload.id).then(function (user) {
+    if (!user) { return res.sendStatus(401); }
+
+    // only update fields that were actually passed...
+    if (typeof req.body.user.username !== 'undefined') {
+      user.username = req.body.user.username;
+    }
+    if (typeof req.body.user.email !== 'undefined') {
+      user.email = req.body.user.email;
+    }
+    if (typeof req.body.user.bio !== 'undefined') {
+      user.bio = req.body.user.bio;
+    }
+    if (typeof req.body.user.image !== 'undefined') {
+      user.image = req.body.user.image;
+    }
+    if (typeof req.body.user.password !== 'undefined') {
+      user.setPassword(req.body.user.password);
+    }
+
+    return user.save().then(function () {
+      return res.json({ user: user.toAuthJSON() });
+    });
   }).catch(next);
 });
 
@@ -37,39 +60,17 @@ router.post('/users/login', function (req, res, next) {
   })(req, res, next);
 });
 
-router.get('/user', auth.required, function (req, res, next) {
-  User.findById(req.payload.id).then(function (user) {
-    if (!user) { return res.sendStatus(401); }
+router.post('/users', function (req, res, next) {
+  var user = new User();
 
+  user.username = req.body.user.username;
+  user.email = req.body.user.email;
+  user.setPassword(req.body.user.password);
+  user.bio = '';
+  user.image = '';
+
+  user.save().then(function () {
     return res.json({ user: user.toAuthJSON() });
-  }).catch(next);
-});
-
-router.put('/user', auth.required, function (req, res, next) {
-  User.findById(req.payload.id).then(function (user) {
-    if (!user) { return res.sendStatus(401); }
-
-    // only update fields that were actually passed...
-
-    if (typeof req.body.user.username !== 'undefined') {
-      user.username = req.body.user.username;
-    }
-    if (typeof req.body.user.email !== 'undefined') {
-      user.email = req.body.user.email;
-    }
-    if (typeof req.body.user.bio !== 'undefined') {
-      user.bio = req.body.user.bio;
-    }
-    if (typeof req.body.user.image !== 'undefined') {
-      user.image = req.body.user.image;
-    }
-    if (typeof req.body.user.password !== 'undefined') {
-      user.setPassword(req.body.user.password);
-    }
-
-    return user.save().then(function () {
-      return res.json({ user: user.toAuthJSON() });
-    });
   }).catch(next);
 });
 
