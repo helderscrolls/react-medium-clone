ad9b8fb1a6d9f9a4e7f4971a386b1a96fe304038
helderscrolls <nortehelder@gmail.com>
2019-08-25 22:23:50 +0200

    Updated models/User.js to accept a user object parameter

--- back/models/User.js
+++ back/models/User.js
@@ -47,7 +47,7 @@ UserSchema.methods.toAuthJSON = function () {
   };
 };
 
-UserSchema.methods.toProfileJSONFor = function () {
+UserSchema.methods.toProfileJSONFor = function (user) {
   return {
     username: this.username,
     bio: this.bio,
