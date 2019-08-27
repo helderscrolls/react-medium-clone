194b29a088073df7ffc835b74b45bf514fbadbf0
helderscrolls <nortehelder@gmail.com>
2019-08-27 13:52:54 +0200

    Added a favorites collection to the UserSchema

--- back/models/User.js
+++ back/models/User.js
@@ -9,6 +9,7 @@ var UserSchema = new mongoose.Schema({
   email: { type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true },
   bio: String,
   image: String,
+  favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Article' }],
   hash: String,
   salt: String
 }, { timestamps: true });
