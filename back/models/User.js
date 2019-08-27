b200f4fc85cf426c72337751533bfc217e4908dd
helderscrolls <nortehelder@gmail.com>
2019-08-27 22:10:25 +0200

    Created a method for a following another user, Created a method for a unfollowing another user, Created a method for checking if a user is following another user

--- back/models/User.js
+++ back/models/User.js
@@ -78,4 +78,24 @@ UserSchema.methods.isFavorite = function (id) {
   });
 };
 
+UserSchema.methods.follow = function (id) {
+  if (this.following.indexOf(id) === -1) {
+    this.following.push(id);
+  }
+
+  return this.save();
+};
+
+UserSchema.methods.unfollow = function (id) {
+  this.following.remove(id);
+
+  return this.save();
+};
+
+UserSchema.methods.isFollowing = function (id) {
+  return this.following.some(function (followId) {
+    return followId.toString() === id.toString();
+  });
+};
+
 mongoose.model('User', UserSchema);
